2378a0c05292c65ac98452a545e20c760180b934
Adarsh M <Adarsh M@TYSS-Adarsh>
2022-01-20 15:35:09 +0530

    second push

---
+++ com.crm.comcast.sdet28/src/test/java/ContactTest/CreateContactWithOrgTest.java
@@ -0,0 +1,11 @@
+package ContactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactWithOrgTest {
+	@Test
+	public void create() {
+		System.out.println("new script");
+	}
+
+}
