a25ad297488c52d5981d04e24f310363a47f8d0c
Adarsh M <Adarsh M@TYSS-Adarsh>
2022-01-20 15:46:26 +0530

    third push

---
+++ com.crm.comcast.sdet28/src/test/java/OrganizationTest/DeleteOrganization.java
@@ -0,0 +1,12 @@
+package OrganizationTest;
+
+import org.testng.annotations.Test;
+
+public class DeleteOrganization {
+	@Test
+	public void de() {
+		
+		System.out.println("delete organization");
+	}
+
+}
